7c4cedb5fd033b2d165e21fcde0466d83b66384e
jhennies <julianhennies@hotmail.de>
2015-11-12 14:56:38 +0100

    Paths in all the three windows

--- boundaryClick/test.clickableBoundaries.py
+++ boundaryClick/test.clickableBoundaries.py
@@ -55,7 +55,8 @@ class ClickablePathItem(QGraphicsPathItem):
                 self.setPen(pen)
 
     def hoverEnterEvent(self, event):
-        pen = QPen(QColor(255, 0, 0))
+        # pen = QPen(QColor(255, 0, 0))
+        pen = self._currentPen
         pen.setWidth(5)
         self.setPen(pen)
 
@@ -72,10 +73,7 @@ class BoundaryViewer(Viewer):
     _layer_back = GrayscaleLayer
     _layer_overlay = ColortableLayer
 
-    _visible_boundaries = []
-    _visible_paths = [[]]
-
-    _clickable_paths = []
+    _clickable_paths = [[]]
 
     def __init__(self):
         super(BoundaryViewer, self).__init__()
@@ -95,43 +93,40 @@ class BoundaryViewer(Viewer):
 
     @pyqtSlot(object, object)
     def slicing_position_changed(self, new_position, old_position):
-        # print "Slicing position changed!"
-        # print new_position
-        # print old_position
-        path_data = self.get_visible_paths(new_position)
-        # print path_data
-        # print len(path_data)
-        # print path_data[0]
-        # print len(path_data[0])
-        # print path_data[1]
-        # print len(path_data[1])
 
-        for o in self._clickable_paths:
-            self.editor.imageScenes[2].removeItem(o)
+        # Calculate paths for each of the three displays
+        for i in range(0, 3):
 
-        self._clickable_paths = []
+            if self._clickable_paths:
+                for p in self._clickable_paths[i]:
+                    self.editor.imageScenes[i].removeItem(p)
+                else:
+                    self._clickable_paths.append([])
 
-        for pd in path_data[1]:
-            # c_x = numpy.array([10, 20, 40, 80]) + 20 * i
-            # c_y = numpy.array([80, 90, 100, 110])
-            c_x = pd[0]
-            c_y = pd[1]
-            print c_x
-            print c_y
-            # path = arrayToQPath(c_x, c_y)
-            self._clickable_paths.append(ClickablePathItem())
-            self._clickable_paths[-1].set_path_from_coordinates(c_x, c_y)
-            # self._clickable_paths[-1].setPath(path)
-            # self._clickable_paths[-1].add_item_to_scene()
-            pen = QPen(QColor(255, 255, 255))
-            pen.setWidth(0)
-            self._clickable_paths[-1].setPen(pen)
+            self._clickable_paths[i] = []
 
-            self.editor.imageScenes[2].addItem(self._clickable_paths[-1])
+            path_data = self.get_visible_paths(new_position, i)
 
-    def get_visible_paths(self, position):
-        current_slice_z = numpy.array(self._overlay_image[0, :, :, position[2], 0], dtype=numpy.float32)
-        return _image_processing.findEdgePaths(current_slice_z)
+            for pd in path_data[1]:
+                c_x = pd[0]
+                c_y = pd[1]
+                self._clickable_paths[i].append(ClickablePathItem())
+                self._clickable_paths[i][-1].set_path_from_coordinates(c_x, c_y)
+                pen = QPen(QColor(255, 255, 255))
+                pen.setWidth(0)
+                self._clickable_paths[i][-1].setPen(pen)
+
+                self.editor.imageScenes[i].addItem(self._clickable_paths[i][-1])
+
+    def get_visible_paths(self, position, display_plane):
+        if display_plane == 0:
+            current_slice = numpy.transpose(numpy.array(self._overlay_image[0, position[0], :, :, 0], dtype=numpy.float32))
+        elif display_plane == 1:
+            current_slice = numpy.array(self._overlay_image[0, :, position[1], :, 0], dtype=numpy.float32)
+        elif display_plane == 2:
+            current_slice = numpy.array(self._overlay_image[0, :, :, position[2], 0], dtype=numpy.float32)
+
+        return _image_processing.findEdgePaths(current_slice)
 
     def determine_edge_pixels(self, label1, label2):
         return 0
@@ -158,17 +153,29 @@ if __name__ == "__main__":
     # v.editor.imageScenes[0]._onSlicingPositionChanged()
 
     # Create images
+    # back = (numpy.ones((100, 200, 300)) * 0).astype(numpy.uint8)
+    # back[:] = 2
+    # back[0:60, 0:60, 0:60] = 1
+    # ol = (numpy.zeros((100, 200, 300))).astype(numpy.uint8)
+    # ol[:] = back
+
     back = (numpy.ones((100, 200, 300)) * 0).astype(numpy.uint8)
+    back[:] = 1
     back[0:60, 0:60, 0:60] = 255
     back[40:80, 40:80, 40:80] = 120
     back[40:100, 40:100, 0:40] = 80
     back[0:45, 50:100, 0:100] = 200
-    back[back == 0] = 1
-
     ol = (numpy.zeros((100, 200, 300))).astype(numpy.uint8)
     # ol[0:99, 0:99, 0:99] = 120
     # ol[0:99, 120:140, 0:99] = 255
     ol[:] = back
+
+    # back = (numpy.ones((20, 20, 20)) * 0).astype(numpy.uint8)
+    # back[:] = 2
+    # back[0:5, 0:5, 0:5] = 1
+    # ol = (numpy.zeros((20, 20, 20))).astype(numpy.uint8)
+    # ol[:] = back
+
     back.shape = (1,)+back.shape+(1,)
     ol.shape = (1,)+ol.shape+(1,)
 
--- boundaryClick/test4.py
+++ boundaryClick/test4.py
@@ -14,12 +14,12 @@ print img, '\n', ret
 print _image_processing.testFunction(1)
 # _my_module.export_HelloWorld()
 
-labels = numpy.zeros((5, 4), dtype=numpy.float32)
-labels[:] = 3
-labels[:, 0] = 1
-labels[0:2, 1] = 1
-labels[0:2, 2:4] = 2
-labels[2, 1:3] = 2
+# labels = numpy.zeros((5, 4), dtype=numpy.float32)
+# labels[:] = 3
+# labels[:, 0] = 1
+# labels[0:2, 1] = 1
+# labels[0:2, 2:4] = 2
+# labels[2, 1:3] = 2
 
 # labels = numpy.zeros((10, 10), dtype=numpy.float32)
 # labels[:] = 1
@@ -27,24 +27,11 @@ labels[2, 1:3] = 2
 # labels[5, 0:3] = 2
 # labels[5, 7:10] = 2
 
+labels = numpy.zeros((10, 10), dtype=numpy.float32)
+labels[:] = 1
+labels[0:5, 0:5] = 2
+# labels[0:5, 5:10] = 1
+
 print labels
 print _image_processing.findEdgePaths(labels)
 
-
-class TestClass:
-
-    _a = 0
-
-    def __init__(self, a):
-        self._a = a
-
-
-tc = TestClass(1)
-
-print tc._a
-
-tc2 = TestClass(1)
-tc2._a = 2
-
-print tc._a
-print tc2._a
