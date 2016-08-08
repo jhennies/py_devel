f741a43227af07b6f4b981615646da8572e46b4e
Julian <julianhennies@hotmail.de>
2016-08-08 11:23:46 +0200

    intital commit of nn upscaling

---
+++ nn_upscale/nn_upscale.py
@@ -0,0 +1,22 @@
+
+import h5py
+import numpy as np
+import re
+from scipy import ndimage
+import random
+# import scipy
+
+__author__ = 'jhennies'
+
+
+class nn_upscale:
+
+    def __init__(self):
+        pass
+
+    def call_nn(self, input=None):
+        return input
+
+
+if __name__ == "__main__":
+    pass
