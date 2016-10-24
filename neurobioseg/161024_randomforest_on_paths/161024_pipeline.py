f8a52190260dd34908070cecb74875e869671886
jhennies <julianhennies@hotmail.de>
2016-10-24 10:44:51 +0200

    test-run of scripts 00 and 02

--- neurobioseg/161024_randomforest_on_paths/161024_pipeline.py
+++ neurobioseg/161024_randomforest_on_paths/161024_pipeline.py
@@ -2,9 +2,11 @@
 __author__ = 'jhennies'
 
 
-# execfile('161005_01_merge_adjacent_objects.py')
-# execfile('161005_02_localmax_on_disttransf.py')
-execfile('161005_03a_paths_within_labels.py')
-execfile('161005_03b_paths_of_partners.py')
+execfile('161024_00_remove_small_objects.py')
+# execfile('161024_01_find_orphans')
+execfile('161024_02_merge_adjacent_objects')
+# execfile('161024_03_localmax_on_disttransf.py')
+# execfile('161005_03a_paths_within_labels.py')
+# execfile('161005_03b_paths_of_partners.py')
 # execfile('161005_04_features_of_paths.py')
 
