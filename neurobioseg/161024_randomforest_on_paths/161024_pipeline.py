7816d77146a952cfb7a5091aaaf3f915655fe558
jhennies <julianhennies@hotmail.de>
2016-10-24 11:07:09 +0200

    re-design of pipeline structure

--- neurobioseg/161024_randomforest_on_paths/161024_pipeline.py
+++ neurobioseg/161024_randomforest_on_paths/161024_pipeline.py
@@ -3,8 +3,8 @@ __author__ = 'jhennies'
 
 
 execfile('161024_00_remove_small_objects.py')
-# execfile('161024_01_find_orphans')
-execfile('161024_02_merge_adjacent_objects.py')
+execfile('161024_01_merge_adjacent_objects.py')
+# execfile('161024_01_find_border_contacts.py')
 # execfile('161024_03_localmax_on_disttransf.py')
 # execfile('161005_03a_paths_within_labels.py')
 # execfile('161005_03b_paths_of_partners.py')
