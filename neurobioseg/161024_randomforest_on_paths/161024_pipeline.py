08aa96a4fbb0d1435f770ab1a7954d3270f902ee
jhennies <julianhennies@hotmail.de>
2016-10-24 10:45:33 +0200

    bug-fix

--- neurobioseg/161024_randomforest_on_paths/161024_pipeline.py
+++ neurobioseg/161024_randomforest_on_paths/161024_pipeline.py
@@ -4,7 +4,7 @@ __author__ = 'jhennies'
 
 execfile('161024_00_remove_small_objects.py')
 # execfile('161024_01_find_orphans')
-execfile('161024_02_merge_adjacent_objects')
+execfile('161024_02_merge_adjacent_objects.py')
 # execfile('161024_03_localmax_on_disttransf.py')
 # execfile('161005_03a_paths_within_labels.py')
 # execfile('161005_03b_paths_of_partners.py')
