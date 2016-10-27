be767073199827101d25e1e532ceb2328e63bc02
jhennies <julianhennies@hotmail.de>
2016-10-27 11:11:02 +0200

    Paths are now found successfully

--- neurobioseg/161024_randomforest_on_paths/161024_04a_paths_within_labels.py
+++ neurobioseg/161024_randomforest_on_paths/161024_04a_paths_within_labels.py
@@ -14,8 +14,44 @@ import processing_lib as lib
 __author__ = 'jhennies'
 
 
+def shortest_paths(indicator, pairs, bounds=None):
+
+    # Crate the grid graph and shortest path objects
+    gridgr = graphs.gridGraph(indicator.shape)
+    indicator = indicator.astype(np.float32)
+    gridgr_edgeind = graphs.edgeFeaturesFromImage(gridgr, indicator)
+    instance = graphs.ShortestPathPathDijkstra(gridgr)
+
+    # Initialize paths image
+    pathsim = np.zeros(indicator.shape)
+    # Initialize list of path coordinates
+    paths = []
+
+    for pair in pairs:
+
+        source = pair[0]
+        target = pair[1]
+
+        targetNode = gridgr.coordinateToNode(target)
+        sourceNode = gridgr.coordinateToNode(source)
+
+        instance.run(gridgr_edgeind, sourceNode, target=targetNode)
+        path = instance.path(pathType='coordinates')
+        # Do not forget to correct for the offset caused by cropping!
+        if bounds is not None:
+            paths.append(path + [bounds[0].start, bounds[1].start, bounds[2].start])
+        else:
+            paths.append(path)
+
+        pathindices = np.swapaxes(path, 0, 1)
+        pathsim[pathindices[0], pathindices[1], pathindices[2]] = 1
+
+    return paths, pathsim
+
+
 def find_shortest_path(hfp, penaltypower, bounds, disttransf, locmax):
 
+    # TODO: distancetransform modifiactation can be done beforehand!
     # Modify distancetransform
     #
     # a) Invert: the lowest values (i.e. the lowest penalty for the shortest path detection) should be at the center of
@@ -32,63 +68,20 @@ def find_shortest_path(hfp, penaltypower, bounds, disttransf, locmax):
     disttransf = lib.power(disttransf, penaltypower)
     # ifp.power(penaltypower, ids='curdisttransf_inf')
 
-    indicator = disttransf
-    # indicator = ifp.get_image('curdisttransf_inf')
-    gridgr = graphs.gridGraph(disttransf.shape)
-
-    hfp.logging('gridgr.shape = {}'.format(gridgr.shape))
-
-    indicator = indicator.astype(np.float32)
-    gridgr_edgeind = graphs.edgeFeaturesFromImage(gridgr, indicator)
-    instance = graphs.ShortestPathPathDijkstra(gridgr)
-
-    # Get two local maxima
+    # Get local maxima
     indices = np.where(locmax)
-    # indices = np.where(ifp.get_image('curlocmax') == 1)
     coords = zip(indices[0], indices[1], indices[2])
     hfp.logging('Local maxima coordinates: {}', coords)
 
-    # ifp.set_data_dict({'pathsim': np.zeros(ifp.get_image('curlocmax').shape)}, append=True)
-    pathsim = np.zeros(locmax.shape)
-
-    hfp.logging('len(coords) = {}', len(coords))
-    paths = []
+    # Make pairwise list of coordinates that will serve as source and target
+    pairs = []
     for i in xrange(0, len(coords)-1):
-
         for j in xrange(i+1, len(coords)):
+            pairs.append((coords[i], coords[j]))
 
-            hfp.logging('---')
-            hfp.logging('i = {}; j = {}', i, j)
+    paths, pathim = shortest_paths(disttransf, pairs, bounds=bounds)
 
-            source = coords[i]
-            target = coords[j]
-
-            targetNode = gridgr.coordinateToNode(target)
-            sourceNode = gridgr.coordinateToNode(source)
-
-            hfp.logging('Source = {}', source)
-            hfp.logging('Target = {}', target)
-
-            instance.run(gridgr_edgeind, sourceNode, target=targetNode)
-            path = instance.path(pathType='coordinates')
-            # Do not forget to correct for the offset caused by cropping!
-            paths.append(path + [bounds[0].start, bounds[1].start, bounds[2].start])
-
-            # for coords in path:
-            #     # ifp.logging('coords = {}'.format(coords))
-            #     pass
-
-            pathindices = np.swapaxes(path, 0, 1)
-            pathsim[pathindices[0], pathindices[1], pathindices[2]] = 1
-
-    # # ifp.concatenate('disttransf', 'paths', target='paths_over_dist')
-    # hfp.astype(np.uint8, ('pathsim', 'curlocmax'))
-    # # ifp.anytask(vigra.filters.multiBinaryDilation, ('paths', 'locmax'), 3)
-    # hfp.swapaxes(0, 2, ids=('pathsim', 'curlocmax', 'curdisttransf'))
-    # hfp.anytask(vigra.filters.discDilation, 2, ids=('pathsim', 'curlocmax'))
-    # hfp.set_data_dict({'paths_over_dist': np.array([ifp.get_image('pathsim'), ifp.get_image('curlocmax'), ifp.get_image('curdisttransf')])}, append=True)
-
-    return paths, pathsim
+    return paths, pathim
 
 
 def paths_within_labels(hfp, key, locmaxkeys, disttransfkey):
@@ -140,59 +133,6 @@ def paths_within_labels(hfp, key, locmaxkeys, disttransfkey):
                                      vigra.filters.multiBinaryDilation(hfp[k].astype(np.uint8), 5)])
 
     return paths
-
-
-# if False:
-#
-#     yamlfile = os.path.dirname(os.path.abspath(__file__)) + '/parameters.yml'
-#
-#     ifp = ImageFileProcessing(
-#         yaml=yamlfile,
-#         yamlspec={'image_path': 'intermedfolder', 'image_file': 'locmaxfile', 'image_names': ('locmaxnames', 0, 2)},
-#         asdict=True,
-#         keys=('disttransf', 'locmax')
-#     )
-#     params = ifp.get_params()
-#     thisparams = params['paths_within_labels']
-#     ifp.addfromfile(params['intermedfolder']+params['largeobjfile'], image_names=params['largeobjname'], ids='largeobj')
-#
-#     ifp.startlogger(filename=params['intermedfolder'] + 'paths_within_labels.log', type='a')
-#
-#     # ifp.code2log(__file__)
-#     ifp.code2log(inspect.stack()[0][1])
-#     ifp.logging('')
-#
-#     ifp.logging('yamlfile = {}', yamlfile)
-#     ifp.logging('ifp.get_data().keys() = {}', ifp.get_data().keys())
-#     ifp.logging('ifp.shape() = {}', ifp.shape())
-#     ifp.logging('{}', ifp.amax())
-#
-#     hfp = Hdf5Processing()
-#     c = 0
-#     for lblo in ifp.label_bounds_iterator('largeobj', 'curlabel', ids=('locmax', 'disttransf'), targetids=('curlocmax', 'curdisttransf'),
-#                                           maskvalue=0, value=0, background=0):
-#
-#         ifp.logging('------------\nCurrent label {} in iteration {}', lblo['label'], c)
-#         ifp.logging('Bounding box = {}', lblo['bounds'])
-#
-#         if ifp.amax('curlocmax') == 1:
-#             ps = find_shortest_path(ifp, thisparams['penaltypower'], lblo['bounds'])
-#             ifp.logging('Number of paths found: {}', len(ps))
-#             if ps:
-#                 hfp.setdata({lblo['label']: ps}, append=True)
-#                 ifp.write(filename='paths_over_dist_true_{}.h5'.format(lblo['label']), ids='paths_over_dist')
-#
-#         else:
-#             ifp.logging('No local maxima found for this label.')
-#
-#         c += 1
-#         # if c == 5:
-#         #     break
-#
-#     hfp.write(filepath=params['intermedfolder']+params['pathstruefile'])
-#
-#     ifp.logging('')
-#     ifp.stoplogger()
 
 
 if __name__ == '__main__':
