e3576462f9a5a5bc2b52ee5fc33641bba7226769
jhennies <julianhennies@hotmail.de>
2016-11-09 16:13:54 +0100

    just the pipeline file

---
+++ neurobioseg/161107_randomforest_on_paths/161024_pipeline.py
@@ -0,0 +1,12 @@
+
+__author__ = 'jhennies'
+
+
+execfile('161024_00_remove_small_objects.py')
+execfile('161024_01_merge_adjacent_objects.py')
+execfile('161024_02_find_border_contacts.py')
+execfile('161024_03_localmax_on_disttransf.py')
+execfile('161024_04a_paths_within_labels.py')
+execfile('161024_04b_paths_of_partners.py')
+# execfile('161024_05_features_of_paths.py')
+
