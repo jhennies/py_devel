8d5a6be4ce2755e4b6a526916673e2b9d76d4dac
jhennies <julianhennies@hotmail.de>
2017-02-02 14:34:10 +0100

    multiprocessing of features working: approx twice as fast

--- neurobioseg/170131_parallel_processing/p170131_01_compute_feature_images.py
+++ neurobioseg/170131_parallel_processing/p170131_01_compute_feature_images.py
@@ -6,6 +6,7 @@ from hdf5_slim_processing import Hdf5Processing as Hp
 import numpy as np
 import processing_lib as lib
 from yaml_parameters import YamlParams
+from concurrent import futures
 
 
 __author__ = 'jhennies'
@@ -37,10 +38,14 @@ class FeatureFunctions:
 
     @staticmethod
     def gaussian(image, general_params, specific_params):
-        return lib.gaussian_smoothing(image, specific_params[0], general_params['anisotropy'])
+        print 'Gaussian: Start...'
+        gaussian = lib.gaussian_smoothing(image, specific_params[0], general_params['anisotropy'])
+        print 'Gaussian: End!'
+        return gaussian
 
     @staticmethod
     def disttransf(image, general_params, specific_params):
+        print 'Distance transform: Start...'
 
         anisotropy = np.array(general_params['anisotropy']).astype(np.float32)
         image = image.astype(np.float32)
@@ -53,53 +58,47 @@ class FeatureFunctions:
         image = image.astype(np.float32)
         image = lib.distance_transform(image, pixel_pitch=anisotropy, background=True)
 
+        print 'Distance transform: End!'
         return image
 
     @staticmethod
     def hessian_eigenvalues(image, general_params, specific_params):
-        return lib.hessian_of_gaussian_eigenvalues(
+        print 'Hessian: Start...'
+        hessian = lib.hessian_of_gaussian_eigenvalues(
             image, specific_params[0], anisotropy=general_params['anisotropy']
         )
+        print 'Hessian: End!'
+        return hessian
 
     @staticmethod
     def structure_tensor_eigenvalues(image, general_params, specific_params):
-        return lib.structure_tensor_eigenvalues(
+        print 'Structure tensor: Start...'
+        structen = lib.structure_tensor_eigenvalues(
             image, specific_params[0], specific_params[1],
             anisotropy=general_params['anisotropy']
         )
+        print 'Structure tensor: End!'
+        return structen
 
     @staticmethod
     def gaussian_gradient_magnitude(image, general_params, specific_params):
-        return lib.gaussian_gradient_magnitude(
+        print 'Gradient magnitude: Start...'
+        mag = lib.gaussian_gradient_magnitude(
             image, specific_params[0],
             anisotropy=general_params['anisotropy']
         )
+        print 'Gradient magniture: End!'
+        return mag
 
     @staticmethod
     def laplacian_of_gaussian(image, general_params, specific_params):
-        return lib.laplacian_of_gaussian(
+        print 'Laplacian: Start...'
+        lapl = lib.laplacian_of_gaussian(
             image, specific_params[0],
             anisotropy=general_params['anisotropy']
         )
-
-
-def compute_features(image, general_params, subfeature_params):
-
-    ff = FeatureFunctions()
-    result = IPL()
-
-    for k, v in subfeature_params.iteritems():
-
-        if v:
-            print 'Computing {}'.format(v['func'])
-            result[k] = getattr(ff, v.pop('func'))(image, general_params, v.pop('params'))
-
-            if len(v) > 0:
-                result[k] = compute_features(result[k], general_params, v)
-        else:
-            result[k] = image
-
-    return result
+        print 'Laplacian: End!'
+        return lapl
 
 
 def experiment_parser(yparams, function, name):
@@ -124,9 +123,65 @@ def experiment_parser(yparams, function, name):
         final = zeroth_defaults.dcp()
         final.merge(experiment)
 
-        compute_feature_images(final, yparams)
+        function(final, yparams)
 
 
+def return_img(image):
+    return image
+
+
+def compute_features_multi(image, general_params, features, logger=None):
+
+    ff = FeatureFunctions()
+    result = Hp()
+
+    if logger is not None:
+        logger.logging('Starting thread pool with a max of {} threads', general_params['max_threads'])
+    with futures.ThreadPoolExecutor(general_params['max_threads']) as do_stuff:
+
+        keys = []
+        vals = []
+        tasks = Rdict()
+
+        for k, v in features.iteritems():
+
+            if v:
+
+                tasks[k] = do_stuff.submit(getattr(ff, v.pop('func')), image, general_params, v.pop('params'))
+                # print 'In the for loop: {}'.format(do_stuff.done())
+                keys.append(k)
+                vals.append(v)
+
+            else:
+                result[k] = image
+
+    for k, v in dict(zip(keys, vals)).iteritems():
+        result[k] = tasks[k].result()
+        if len(v) > 0:
+            result[k] = compute_features_multi(result[k], general_params, v)
+
+    return result
+
+
+def compute_features(image, general_params, features):
+
+    ff = FeatureFunctions()
+    result = Hp()
+
+    for k, v in features.iteritems():
+
+        if v:
+            print 'Computing {}'.format(v['func'])
+            result[k] = getattr(ff, v.pop('func'))(image, general_params, v.pop('params'))
+
+            if len(v) > 0:
+                result[k] = compute_features(result[k], general_params, v)
+        else:
+            result[k] = image
+
+    return result
+
+from timeit import default_timer as timer
 def compute_feature_images(experiment, yparams):
 
     all_params = yparams.get_params()
@@ -140,7 +195,7 @@ def compute_feature_images(experiment, yparams):
     if len(source) > 2:
         kw_load = source[2]
     data = load_images(
-        all_params[source[0]] + params[source[1]], logger=yparams, **kw_load
+        all_params[source[0]] + all_params[source[1]], logger=yparams, **kw_load
     )
 
     yparams.logging('\nInitial datastructure: \n\n{}', data.datastructure2string(maxdepth=3))
@@ -148,6 +203,40 @@ def compute_feature_images(experiment, yparams):
     # Set target file
     target_file = all_params[target[0]] + all_params[target[1]]
 
+    # # TODO: Parallelize here
+    # with futures.ThreadPoolExecutor as do_stuff:
+    #     tasks = []
+    #     for d, k, v, kl in data.data_iterator(yield_short_kl=True, leaves_only=True):
+    #         tasks.append(do_stuff.submit(compute_features(v, params, )))
+    #
+    #     result = [x.result() for x in tasks]
+
+    for d, k, v, kl in data.data_iterator(leaves_only=True):
+        yparams.logging('===============================\nWorking on image: {}', kl)
+
+        # Load data into memory
+        data[kl] = np.array(v)
+
+        # Feature calculation
+        start = timer()
+        data[kl] = compute_features_multi(data[kl], params, features, logger=yparams)
+        elapsed = timer()
+        elapsed = elapsed - start
+        print "Time spent in compute_features_multi is: ", elapsed
+
+        # start = timer()
+        # data[kl] = compute_features(data[kl], params, features)
+        # elapsed = timer()
+        # elapsed = elapsed - start
+        # print "Time spent in compute_features is: ", elapsed
+
+        # Write result
+        data.write(target_file, keys=kl)
+        # Free memory
+        data[kl] = None
+
+    # Close file
+    data.close()
 
 
 def run_compute_feature_images(yamlfile):
@@ -171,7 +260,7 @@ def run_compute_feature_images(yamlfile):
         yparams.stoplogger()
 
     except:
-
+        raise
         yparams.errout('Unexpected error')
 
 
